5770802e19b6c6b64405ea797543fa64285634b2
xuwei <Ayu.start@outlook.com>
2019-09-06 17:13:06 +0800

    East Message

---
+++ SCEEC.MI.EAST/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Stylet;
+using StyletIoC;
+using SCEEC.MI.EAST.Pages;
+
+namespace SCEEC.MI.EAST
+{
+    public class Bootstrapper : Bootstrapper<ViewModels.MainViewModel>
+    {
+        protected override void ConfigureIoC(IStyletIoCBuilder builder)
+        {
+            // Configure the IoC container in here
+        }
+
+        protected override void Configure()
+        {
+            // Perform any other configuration before the application starts
+        }
+
+       
+    }
+}
---
+++ SCEEC.MI.EAST/Pages/ShellViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Stylet;
+
+namespace SCEEC.MI.EAST.Pages
+{
+    public class ShellViewModel : Screen
+    {
+    }
+}
